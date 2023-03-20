84609af7edf63c8609cb4eac8f2cc1f1ca980819
OshilimEO <eoluwakorede@yahoo.com>
2023-03-19 18:52:21 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 /**
-* main - main entry point
-* return: Always 0 (Success)
+* main - A c file to print a string to the stdout using a puts.
+* put - a function to print to stdout.
+* Return - 0 signifying program success.
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+	/**
+	* return: Always 0 (Success)
+	*/
 	return (0);
 }
