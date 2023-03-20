5df733ad38e055ea33247e234cb00694a456d43e
OshilimEO <eoluwakorede@yahoo.com>
2023-03-20 01:34:56 -0700

    Output a STMT using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 /**
-* main - main entry point
-* return: Always 0 (Success)
+ * main - main entry point
+ *
+ * Return:: 0 signifying program success.
 */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+	/**
+	  * Return:: always 0 (Success)
+	*/
 	return (0);
 }
