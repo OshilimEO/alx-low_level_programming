5d5eeffd3b0dd4cca02f33e2b50a6845ab068860
OshilimEO <eoluwakorede@yahoo.com>
2023-03-27 22:02:32 -0700

    Half the lies told about me are not true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+  * puts2 - function should print only one character out of 2
+  * starting with the first one
+  * @str: input
+  * Return: print
+  */
+
+void puts2(char *str)
+{
+	int longi = 0;
+	int i = 0;
+	char *y = str;
+	int o;
+
+	while (*y != '\0')
+	{
+		y++;
+		longi++;
+	}
+	t = longi - 1;
+	for (o = 0; o <= t; o++)
+	{
+		if (o % 2 == 0)
+		{
+			_putchar(str[o]);
+		}
+		_putchar('\n');
+	}
+}
